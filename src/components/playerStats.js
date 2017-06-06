905dd36534711b19faab4951ea4189e90c1ffb92
Cam <camiloscamp@gmail.com>
2017-06-06 13:49:15 -0400

    Added style for player texts

--- src/components/playerStats.js
+++ src/components/playerStats.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const PlayerStats = ({player, position}) => (
   <div className="player-stats">
-    <h1>Name: {player.name} - [x: {position.x} y: {position.y}]</h1>
-    <h1>HP: {player.hp}</h1>
+    <h1 className="player-stats__text player-stats--text ">Name: {player.name} - [x: {position.x} y: {position.y}]</h1>
+    <h1 className="player-stats__text player-stats--text ">HP: {player.hp}</h1>
   </div>
 )
 
