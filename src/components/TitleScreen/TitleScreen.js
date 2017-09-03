6aef12fd1a3e2160a3d6849f40f220c4fbf063bc
Cam <c4miloei@gmail.com>
2017-09-02 17:16:59 -0700

    Add title screen

--- src/components/TitleScreen/TitleScreen.js
+++ src/components/TitleScreen/TitleScreen.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 class TitleScreen extends Component {
+  static propTypes = {
+    onStartPressed: PropTypes.func.isRequired,
+  };
+
   render() {
+    const { onStartPressed } = this.props;
+
     return (
       <div className="title-screen">
-        <p>title screen</p>
+        <div className="title-screen__name">
+          <p>x99</p>
+        </div>
+        <div className="title-screen__tap" onClick={onStartPressed}>
+          Click to start
+        </div>
       </div>
     );
   }
--- src/containers/TitleScreen/TitleScreen.container.js
+++ src/containers/TitleScreen/TitleScreen.container.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import TitleScreen from '../../components/TitleScreen/TitleScreen';
 
+import { onSetCurrentView } from '../MainFrame/MainFrame.actions';
+
 class TitleScreenContainer extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func,
+  };
+
   constructor(props) {
     super(props);
+
+    this.onStartPressed = this.onStartPressed.bind(this);
+  }
+
+  onStartPressed() {
+    const { dispatch } = this.props;
+    dispatch(onSetCurrentView('SelectionScreen'));
   }
 
   render() {
-    return <TitleScreen />;
+    return <TitleScreen onStartPressed={this.onStartPressed} />;
   }
 }
 
-export default TitleScreenContainer;
+const TitleScreenConnect = connect(mapStateToProps)(TitleScreenContainer);
+
+function mapStateToProps(state) {
+  return {
+    ...state,
+  };
+}
+
+export default TitleScreenConnect;
