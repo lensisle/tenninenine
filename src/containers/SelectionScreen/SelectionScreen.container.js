54f2cf6bcac587fbb888f36c6938585bb48715cc
Cam <c4miloei@gmail.com>
2017-09-02 17:19:13 -0700

    Add selection screen empty container

---
+++ src/containers/SelectionScreen/SelectionScreen.container.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class SelectionScreenContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>Selection Screen</p>;
+  }
+}
+
+export default SelectionScreenContainer;
